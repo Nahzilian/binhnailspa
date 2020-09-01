0cd88192fd2a3f41d802aeb7f1215bb8580e3689
Nahzilian <duc.nguyen@ryerson.ca>
2020-08-31 23:10:32 -0400

    final

--- binhnailfe2/components/slide.js
+++ binhnailfe2/components/slide.js
@@ -1,18 +1,3 @@
-var slideIndex = 0;
-slide();
-
-function slide() {
-    var i =0;
-    var slides = document.getElementsByClassName("slides")
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) { slideIndex = 1 }
-    slides[slideIndex - 1].style.display = "block";
-    setTimeout(slide, 6000); // Change image every 2 seconds
-}
-
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
